f148d68dce8293923200b242cb86d2e54c326e4c
Mid2oceans <46832094+Mid2oceans@users.noreply.github.com>
2022-02-15 14:58:53 -0500

    inital base

---
+++ server.js
@@ -0,0 +1,23 @@
+const PORT = 8000
+var express = require('express');
+var app = express();
+const cors = require('cors');
+app.use(cors())
+
+app.set('view engine','ejs')
+
+let hashWord
+app.get('/word/:wordId',(req, res,next) =>{
+    // console.log(tools.unhash(req.params.wordId));
+    hashWord = req.params.wordId;
+    // res.send("hi")
+    res.render('index');
+    next()
+});
+
+app.get('/answer123', (req,res) =>{
+
+    res.json({hashWord})
+})
+
+app.listen(PORT);
